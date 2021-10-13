9873d5beecb064d6b801fbb4c463de35cf003ad9
Bhavya Tomar <58269749+BhavyaCodes@users.noreply.github.com>
2021-10-14 01:33:20 +0530

    delete original file after converting (#3)

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,12 +1,18 @@
 import {
   BadRequestException,
   Controller,
+  InternalServerErrorException,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MediaService } from './media.service';
+import * as ffmpeg from 'fluent-ffmpeg';
+import * as fs from 'fs';
+import { join } from 'path';
+import { Response } from 'express';
 
 @Controller('media')
 export class MediaController {
@@ -18,7 +24,8 @@ export class MediaController {
         if (
           file.mimetype === 'video/x-matroska' || //mkv
           file.mimetype === 'video/webm' || //webm
-          file.mimetype === 'video/x-m4v' //m4v
+          file.mimetype === 'video/x-m4v' || //m4v
+          file.mimetype === 'video/mp4' //mp4
         ) {
           cb(null, true);
         } else {
@@ -27,10 +34,31 @@ export class MediaController {
       },
     }),
   )
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
-    // return file.filename;
-    // return file.originalname;
-    return file;
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Res() res: Response) {
+    console.log(file.path);
+
+    const originalPath = join(__dirname, '..', '..', file.path);
+    const mp4Path = join(__dirname, '..', '..', file.path + '.mp4');
+    const command = ffmpeg(originalPath);
+
+    command.toFormat('mp4');
+    command.saveToFile(mp4Path);
+
+    command.on('progress', function (progress) {
+      console.log('Processing: ' + progress.percent + '% done');
+    });
+    command.on('end', () => {
+      console.log('finished converting');
+      res.send({ ...file, path: `/${file.path}.mp4` });
+      fs.unlink(originalPath, (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    });
+    command.on('error', () => {
+      throw new InternalServerErrorException('Error when converting');
+    });
   }
 }
 
