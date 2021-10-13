f71be2db79beb42dce0249d19bad69f00563fbc6
Bhavya <xbhavyatomar@gmail.com>
2021-10-13 23:22:27 +0530

    finish logic to upload video

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   Controller,
   Post,
   UploadedFile,
@@ -6,13 +7,47 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MediaService } from './media.service';
+import { v4 as uuidv4 } from 'uuid';
+import * as multer from 'multer';
 
 @Controller('media')
 export class MediaController {
   constructor(private readonly mediaService: MediaService) {}
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: (req: Request, file: Express.Multer.File, cb) => {
+        if (
+          file.mimetype === 'video/x-matroska' || //mkv
+          file.mimetype === 'video/webm' || //webm
+          file.mimetype === 'video/x-m4v' //m4v
+        ) {
+          cb(null, true);
+        } else {
+          cb(new BadRequestException('Invalid File'), false);
+        }
+      },
+    }),
+  )
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    // return file.filename;
+    // return file.originalname;
+    return file;
   }
 }
+
+// @UseInterceptors(
+// 	FileInterceptor('file', {
+// 		fileFilter: (req: Request, file: Express.Multer.File, cb) => {
+// 			if (
+// 				file.mimetype === 'video/x-matroska' ||
+// 				file.mimetype === 'video/webm' ||
+// 				file.mimetype === 'video/x-m4v'
+// 			) {
+// 				cb(null, true);
+// 			} else {
+// 				cb(null, false);
+// 			}
+// 		},
+// 	}),
+// )
--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { MediaService } from './media.service';
 import { MediaController } from './media.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { join } from 'path';
+import { mkdirSync } from 'fs';
+import { v4 } from 'uuid';
 
 @Module({
+  imports: [
+    MulterModule.register({
+      dest: './upload',
+      storage: multer.diskStorage({
+        destination(req, file, cb) {
+          mkdirSync('./upload', { recursive: true });
+          // mkdirSync('./upload');
+          // cb(null, join(__dirname, './upload'));
+          cb(null, './upload');
+        },
+        filename(req, file, cb) {
+          cb(null, v4() + file.originalname);
+        },
+      }),
+    }),
+  ],
   providers: [MediaService],
-  controllers: [MediaController]
+  controllers: [MediaController],
 })
 export class MediaModule {}
