02c31e7093bcb0a3ed03e74954d9f5bf40e51924
Bhavya <xbhavyatomar@gmail.com>
2021-10-13 12:07:21 +0530

    generate files using cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CitiesModule } from './cities/cities.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/pod'),
     CitiesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
