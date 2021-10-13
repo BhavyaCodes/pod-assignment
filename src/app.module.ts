56d0912d597f1df301915cd2b82163b921675c13
Bhavya <xbhavyatomar@gmail.com>
2021-10-14 01:53:35 +0530

    static serve uploaded videos

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,20 @@ import { CitiesModule } from './cities/cities.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MediaModule } from './media/media.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/pod'),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'upload'),
+      serveStaticOptions: {
+        index: false,
+      },
+      // renderPath: '/upload',
+      serveRoot: '/upload',
+    }),
     CitiesModule,
     AuthModule,
     UsersModule,
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // console.log(__dirname);
+  // console.log(join(__dirname, '..', 'upload'));
+  // app.use(express.static(join(__dirname, '..', 'upload')));
   app.setGlobalPrefix('api');
   await app.listen(5000);
 }
