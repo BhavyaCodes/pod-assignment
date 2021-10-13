876546c979d66375b4cbb950797e84f2875fe456
Bhavya <xbhavyatomar@gmail.com>
2021-10-13 21:37:21 +0530

    fix conflict

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,26 +2,23 @@ import './App.css';
 import CityTable from './components/CityTable';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Login, Signup } from './pages';
-import { UserProvider } from './context/user.context';
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <UserProvider>
-            <Route path="/" exact>
-              <h1>Home</h1>
-            </Route>
-            <Route path="/signup" exact>
-              <Signup />
-            </Route>
-            <Route path="/login" exact>
-              <Login />
-            </Route>
-            <Route path="/cities" exact>
-              <CityTable />
-            </Route>
-          </UserProvider>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/signup" exact>
+            <Signup />
+          </Route>
+          <Route path="/login" exact>
+            <Login />
+          </Route>
+          <Route path="/cities" exact>
+            <CityTable />
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
